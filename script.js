061b77ffb8dac9e6a93b4cfc78edf2b773ac26de
QasimKhan190 <qasimkamran17@gmail.com>
2025-02-09 21:34:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,194 @@
+h=document.getElementById('h1')
+
+
+function speak(text){
+    const text_speak = new SpeechSynthesisUtterance(text);
+
+    text_speak.rate = 1;
+    text_speak.volume = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+
+
+function q() {
+        qarr = ["Queen", "Quick", "Quiet"]; 
+        qm = qarr[Math.floor(Math.random() * qarr.length)]; 
+        d=h.innerHTML=(qm); 
+        speak(d) 
+}
+    
+function w() {
+        warr = ["Water", "Window", "Winter","Wonder"];  
+        wm = warr[Math.floor(Math.random() * warr.length)]; 
+        d=h.innerHTML=(wm);
+        speak(d)  
+}
+function e() {
+        earr = ["Elephant", "Energy", "Exercise","Example"];  
+        em = earr[Math.floor(Math.random() * earr.length)]; 
+        d=h.innerHTML=(em);  
+        speak(d)  
+}
+function r() {
+        rarr = ["Rain", "Rocket", "River","Rainbow"];  
+        rm = rarr[Math.floor(Math.random() * rarr.length)]; 
+        d=h.innerHTML=(rm);  
+        speak(d)  
+}
+function t() {
+        t1 = ["Tiger", "Travel", "Table","Teacher"];  
+       t2 = t1[Math.floor(Math.random() * t1.length)]; 
+        d=h.innerHTML=(t2);  
+        speak(d)  
+}
+    
+function y() {
+        y1 = ["Yellow", "Young", "Yummy","Yoga"];  
+        y2 = y1[Math.floor(Math.random() * y1.length)]; 
+        d=h.innerHTML=(y2);  
+        speak(d)  
+}
+    
+    function u() {
+            u1 = ["Umbrella", "Uniform", "Unique","Universe"];  
+            u2 = u1[Math.floor(Math.random() * u1.length)]; 
+            d=h.innerHTML=(u2);  
+            speak(d)  
+}
+        
+ function i() {
+     i1 = ["Important", "Industry", "Intelligent","Increase"];  
+      i2 = i1[Math.floor(Math.random() * i1.length)]; 
+      d=h.innerHTML=(i2);  
+      speak(d)  
+}
+
+ function o() {
+     o1 = ["Ocean", "Orange", "Order","Open"];  
+      o2 = o1[Math.floor(Math.random() * o1.length)]; 
+      d=h.innerHTML=(o2);  
+      speak(d)  
+}
+
+ function p() {
+     p1 = ["Peace", "Power", "Perfect","Positive"];  
+      p2 = p1[Math.floor(Math.random() * p1.length)]; 
+      d=h.innerHTML=(p2);  
+      speak(d)  
+}
+
+ function a() {
+     a1 = ["Apple", "Amazing", "Active","Adventure"];  
+      a2 = a1[Math.floor(Math.random() * a1.length)]; 
+      d=h.innerHTML=(a2);  
+      speak(d)  
+}
+
+ function s() {
+     s1 = ["Smile", "Smart", "Success","Simple"];  
+      s2 = s1[Math.floor(Math.random() * s1.length)]; 
+      d=h.innerHTML=(s2);  
+      speak(d)  
+}
+
+function df() {
+    d1 = ["Dream", "Dance", "Delight","Discover"];  
+    d2 = d1[Math.floor(Math.random() * d1.length)]; 
+    d=h.innerHTML=(d2);  
+    speak(d)  
+}
+
+ function f() {
+     f1 = ["Freedom", "Faith", "Flourish", "Fantasy"];  
+      f2 = f1[Math.floor(Math.random() * f1.length)]; 
+      d=h.innerHTML=(f2);  
+      speak(d)  
+}
+
+ function g() {
+     g1 = ["Green", "Glory", "Galaxy", "Gift"];  
+      g2 = g1[Math.floor(Math.random() * g1.length)]; 
+      d=h.innerHTML=(g2); 
+      speak(d)  
+}
+
+ function hf() {
+    h1 = ["Happy", "Hero", "Honor", "Hope"];  
+    h2 = h1[Math.floor(Math.random() * h1.length)];
+    console.log(h2);
+    d=h.innerHTML=(h2); 
+    speak(d)  
+}
+
+ function j() {
+     j1 = ["Joy", "Journey", "Jungle", "Jump"];  
+      j2 = j1[Math.floor(Math.random() * j1.length)]; 
+      d=h.innerHTML=(j2); 
+      speak(d)  
+}
+
+ function k() {
+     k1 = ["King", "Kind", "Kite", "Knowledge"];  
+      k2 = k1[Math.floor(Math.random() * k1.length)]; 
+      d=h.innerHTML=(k2); 
+      speak(d)  
+}
+
+ function l() {
+     l1 = ["Love", "Light", "Laugh", "Lion"];  
+      l2 = l1[Math.floor(Math.random() * l1.length)]; 
+      d=h.innerHTML=(l2); 
+      speak(d)  
+}
+
+ function z() {
+     z1 = ["Zebra", "Zoom", "Zigzag", "Zero"];  
+      z2 = z1[Math.floor(Math.random() * z1.length)]; 
+      d=h.innerHTML=(z2); 
+      speak(d)  
+}
+
+ function x() {
+     x1 = ["Xenon", "Xylophone", "X-ray", "Xenophobe"];  
+      x2 = x1[Math.floor(Math.random() * x1.length)]; 
+      d=h.innerHTML=(x2); 
+      speak(d)  
+}
+
+ function c() {
+     c1 = ["Cat", "Courage", "City", "Challenge"];  
+      c2 = c1[Math.floor(Math.random() * c1.length)]; 
+      d=h.innerHTML=(c2); 
+      speak(d)  
+}
+
+ function v() {
+     v1 = ["Victory", "Venture", "vigo", "Voice"];  
+      v2 = v1[Math.floor(Math.random() * v1.length)]; 
+      d=h.innerHTML=(v2); 
+      speak(d)  
+}
+
+ function b() {
+     b1 = ["Brave", "Beautiful", "Balance", "bless"];  
+      b2 = b1[Math.floor(Math.random() * b1.length)]; 
+      d=h.innerHTML=(b2); 
+      speak(d)  
+}
+ function n() {
+     n1 = ["Nature", "Noble", "Novel", "Navigate"];  
+      n2 = n1[Math.floor(Math.random() * n1.length)]; 
+      d=h.innerHTML=(n2); 
+      speak(d)  
+}
+ function m() {
+     m1 = ["Mystery", "Magical", "Movement", "Mountain"];  
+      m2 = m1[Math.floor(Math.random() * m1.length)]; 
+      d=h.innerHTML=(m2); 
+      speak(d)  
+}
+
+
